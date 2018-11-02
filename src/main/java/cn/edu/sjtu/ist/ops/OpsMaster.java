05de9d0d485c4557339476a7e8575df1d5df9ce9
rudeigerc <rudeigerc@gmail.com>
2018-11-02 16:42:46 +0800

    Update plugins

--- src/main/java/cn/edu/sjtu/ist/ops/OpsMaster.java
+++ src/main/java/cn/edu/sjtu/ist/ops/OpsMaster.java
@@ -1,11 +1,12 @@
 package cn.edu.sjtu.ist.ops;
 
 import cn.edu.sjtu.ist.ops.util.*;
+
 public class OpsMaster {
     public static void main(String[] args) throws InterruptedException {
         EtcdService.initClient();
         EtcdService.put("test", "test");
-        while(true) {
+        while (true) {
             System.out.println("hello master.");
             Thread.sleep(5000);
         }
