bc1d65a1206fc9b2f8fcd3a67bb0764f49ab137c
rudeigerc <rudeigerc@gmail.com>
2018-11-02 18:09:59 +0800

    Config replacement

--- src/main/java/cn/edu/sjtu/ist/ops/OpsMaster.java
+++ src/main/java/cn/edu/sjtu/ist/ops/OpsMaster.java
@@ -1,11 +1,11 @@
 package cn.edu.sjtu.ist.ops;
 
-import cn.edu.sjtu.ist.ops.util.*;
+import cn.edu.sjtu.ist.ops.util.EtcdService;
 
 public class OpsMaster {
     public static void main(String[] args) throws InterruptedException {
         EtcdService.initClient();
-        EtcdService.put("test", "test");
+        // EtcdService.put("test", "test");
         while (true) {
             System.out.println("hello master.");
             Thread.sleep(5000);
--- src/main/java/cn/edu/sjtu/ist/ops/util/EtcdService.java
+++ src/main/java/cn/edu/sjtu/ist/ops/util/EtcdService.java
@@ -1,12 +1,14 @@
 package cn.edu.sjtu.ist.ops.util;
 
-import com.coreos.jetcd.Client;
-import com.coreos.jetcd.data.KeyValue;
-import com.coreos.jetcd.data.ByteSequence;
-import com.coreos.jetcd.lease.*;
-import com.coreos.jetcd.options.*;
-
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
+
+import com.coreos.jetcd.Client;
+import com.coreos.jetcd.data.ByteSequence;
+import com.coreos.jetcd.data.KeyValue;
+import com.coreos.jetcd.lease.LeaseGrantResponse;
+import com.coreos.jetcd.options.PutOption;
 
 public class EtcdService {
     private static Client client = null;
@@ -14,14 +16,22 @@ public class EtcdService {
     public static Client getClient() {
         return client;
     }
- 
+
     /**
      * 
      */
     public static synchronized void initClient() {
         if (null == client) {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            Properties props = new Properties();
+            try (InputStream resourceStream = loader.getResourceAsStream("ops.properties")) {
+                props.load(resourceStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-            client = Client.builder().endpoints("http://202.120.40.4:12379").build();
+            client = Client.builder()
+                    .endpoints(props.get("etcd.host").toString() + ":" + props.get("etcd.port").toString()).build();
         }
     }
 
@@ -39,7 +49,7 @@ public class EtcdService {
         }
         return keyValue;
     }
- 
+
     /**
      * 
      * @param key
