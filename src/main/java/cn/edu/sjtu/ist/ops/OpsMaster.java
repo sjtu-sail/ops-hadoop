c2f6a69c881209ac2141a77001c70296a2466c78
Wu, ChungHsuan <wuchunghsuan@gmail.com>
2018-11-02 14:11:41 +0800

    Add EtcdService

--- src/main/java/cn/edu/sjtu/ist/ops/OpsMaster.java
+++ src/main/java/cn/edu/sjtu/ist/ops/OpsMaster.java
@@ -1,4 +1,10 @@
 package cn.edu.sjtu.ist.ops;
 
 public class OpsMaster {
+    public static void main(String[] args) throws InterruptedException {
+        while(true) {
+            System.out.println("hello master.");
+            Thread.sleep(5000);
+        }
+    }
 }
---
+++ src/main/java/cn/edu/sjtu/ist/ops/util/EtcdService.java
@@ -0,0 +1,80 @@
+package cn.edu.sjtu.ist.ops.util;
+
+import com.coreos.jetcd.Client;
+import com.coreos.jetcd.data.KeyValue;
+import com.coreos.jetcd.data.ByteSequence;
+import com.coreos.jetcd.lease.*;
+import com.coreos.jetcd.options.*;
+
+import java.util.concurrent.CompletableFuture;
+
+public class EtcdService {
+    private static Client client = null;
+
+    public static Client getClient() {
+        return client;
+    }
+ 
+    /**
+     * 
+     */
+    public static synchronized void initClient() {
+        if (null == client) {
+
+            client = Client.builder().endpoints("http://" + "127.0.0.1:2379").build();
+        }
+    }
+
+    /**
+     * 
+     * @param key
+     * @return
+     */
+    public static KeyValue get(String key) {
+        KeyValue keyValue = null;
+        try {
+            keyValue = client.getKVClient().get(ByteSequence.fromString(key)).get().getKvs().get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return keyValue;
+    }
+ 
+    /**
+     * 
+     * @param key
+     * @param value
+     */
+    public static void put(String key, String value) {
+        client.getKVClient().put(ByteSequence.fromString(key), ByteSequence.fromString(value));
+    }
+
+    /**
+     * 
+     * @param key
+     * @param value
+     * @param ttl
+     * @return
+     */
+    public static long lease(String key, String value, long ttl) {
+        CompletableFuture<LeaseGrantResponse> leaseGrantResponse = client.getLeaseClient().grant(ttl);
+        PutOption putOption;
+        try {
+            putOption = PutOption.newBuilder().withLeaseId(leaseGrantResponse.get().getID()).build();
+            client.getKVClient().put(ByteSequence.fromString(key), ByteSequence.fromString(value), putOption);
+            return leaseGrantResponse.get().getID();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0L;
+    }
+
+    /**
+     * 
+     * @param leaseId
+     */
+    public static void keepAlive(long leaseId) {
+        getClient().getLeaseClient().keepAlive(leaseId);
+    }
+
+}
