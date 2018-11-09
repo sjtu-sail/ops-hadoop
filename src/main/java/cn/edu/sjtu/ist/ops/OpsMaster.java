58dc42d52008166ab9d95359df3a3d3e768eb90c
rudeigerc <rudeigerc@gmail.com>
2018-11-10 00:49:29 +0800

    Integrate gRPC demo

--- src/main/java/cn/edu/sjtu/ist/ops/OpsMaster.java
+++ src/main/java/cn/edu/sjtu/ist/ops/OpsMaster.java
@@ -1,18 +1,59 @@
 package cn.edu.sjtu.ist.ops;
 
+import java.io.IOException;
 import java.net.InetAddress;
 
 import com.google.gson.Gson;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cn.edu.sjtu.ist.ops.common.OpsNode;
 import cn.edu.sjtu.ist.ops.util.EtcdService;
 import cn.edu.sjtu.ist.ops.util.HeartbeatThread;
 import cn.edu.sjtu.ist.ops.util.WatcherThread;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
 
 public class OpsMaster {
+
+    private static final Logger logger = LoggerFactory.getLogger(OpsMaster.class);
+    private final int port;
+    private final Server server;
+
+    public OpsMaster(int port) {
+        this.port = port;
+        this.server = ServerBuilder.forPort(port).addService(new OpsInternalService()).build();
+    }
+
+    public void start() throws IOException {
+        server.start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                OpsMaster.this.stop();
+            }
+        });
+    }
+
+    public void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        OpsMaster opsMaster = new OpsMaster(14000);
         EtcdService.initClient();
-        // EtcdService.put("ops/test", "test");
+
         try {
             InetAddress addr = InetAddress.getLocalHost();
             OpsNode master = new OpsNode(addr.getHostAddress(), addr.getHostName());
@@ -21,8 +62,34 @@ public class OpsMaster {
             WatcherThread watcherThread = new WatcherThread("ops/nodes/worker");
             heartbeatThread.start();
             watcherThread.start();
+
+            opsMaster.start();
+            opsMaster.blockUntilShutdown();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static class OpsInternalService extends OpsInternalGrpc.OpsInternalImplBase {
+        @Override
+        public StreamObserver<OpsRequest> exchange(StreamObserver<OpsResponse> responseObserver) {
+            return new StreamObserver<OpsRequest>() {
+                @Override
+                public void onNext(OpsRequest request) {
+                    responseObserver.onNext(OpsResponse.newBuilder().setMsg("OpsMaster").build());
+                    logger.debug("OpsMaster: " + request.getMsg());
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    logger.warn("Encountered error in exchange", t);
+                }
+
+                @Override
+                public void onCompleted() {
+                    responseObserver.onCompleted();
+                }
+            };
         }
     }
 }
--- src/main/java/cn/edu/sjtu/ist/ops/OpsWorker.java
+++ src/main/java/cn/edu/sjtu/ist/ops/OpsWorker.java
@@ -1,24 +1,94 @@
 package cn.edu.sjtu.ist.ops;
 
 import java.net.InetAddress;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import com.google.gson.Gson;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cn.edu.sjtu.ist.ops.common.OpsNode;
 import cn.edu.sjtu.ist.ops.util.EtcdService;
 import cn.edu.sjtu.ist.ops.util.HeartbeatThread;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
 public class OpsWorker {
+
+    private static final Logger logger = LoggerFactory.getLogger(OpsWorker.class);
+    private final ManagedChannel channel;
+    private final OpsInternalGrpc.OpsInternalStub asyncStub;
+
+    public OpsWorker(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+    }
+
+    public OpsWorker(ManagedChannelBuilder<?> channelBuilder) {
+        this.channel = channelBuilder.build();
+        this.asyncStub = OpsInternalGrpc.newStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public CountDownLatch routeChat() {
+        final CountDownLatch finishLatch = new CountDownLatch(1);
+        StreamObserver<OpsRequest> requestObserver = asyncStub.exchange(new StreamObserver<OpsResponse>() {
+            @Override
+            public void onNext(OpsResponse response) {
+                logger.debug("OpsWorker: " + response.getMsg());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                finishLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                finishLatch.countDown();
+            }
+        });
+
+        try {
+            OpsRequest request = OpsRequest.newBuilder().setMsg("OpsWorker").build();
+            requestObserver.onNext(request);
+        } catch (RuntimeException e) {
+            // Cancel RPC
+            requestObserver.onError(e);
+            throw e;
+        }
+        // Mark the end of requests
+        requestObserver.onCompleted();
+
+        // return the latch while receiving happens asynchronously
+        return finishLatch;
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        OpsWorker opsWorker = new OpsWorker("localhost", 14000);
         EtcdService.initClient();
+
         try {
             InetAddress addr = InetAddress.getLocalHost();
             OpsNode worker = new OpsNode(addr.getHostAddress(), addr.getHostName());
             Gson gson = new Gson();
             HeartbeatThread thread = new HeartbeatThread("ops/nodes/worker/", gson.toJson(worker));
             thread.start();
+
+            CountDownLatch finishLatch = opsWorker.routeChat();
+
+            if (!finishLatch.await(1, TimeUnit.MINUTES)) {
+                logger.warn("exchange can not finish within 1 minutes");
+            }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            opsWorker.shutdown();
         }
     }
 }
