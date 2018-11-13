e82c74879396f8837f8aad4bce0507ca18eaaa28
rudeigerc <rudeigerc@gmail.com>
2018-11-13 15:14:20 +0800

    Update conf

--- src/main/java/cn/edu/sjtu/ist/ops/util/OpsConf.java
+++ src/main/java/cn/edu/sjtu/ist/ops/util/OpsConf.java
@@ -1,19 +1,70 @@
 package cn.edu.sjtu.ist.ops.util;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
 
 public class OpsConf {
     @JsonProperty
+    private Ops ops;
+
+    @JsonProperty
     private Etcd etcd;
+
+    public Ops getOps() {
+        return ops;
+    }
 
     public Etcd getEtcd() {
         return etcd;
     }
+}
 
-    public void setEtcd(Etcd etcd) {
-        this.etcd = etcd;
+class Ops {
+    @JsonProperty
+    private Master master;
+
+    @JsonProperty
+    private Grpc grpc;
+
+    public Master getMaster() {
+        return master;
+    }
+
+    public Grpc getGrpc() {
+        return grpc;
+    }
+}
+
+class Master {
+    @JsonProperty
+    private String hostname;
+
+    @JsonProperty
+    private Integer port;
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+}
+
+class Grpc {
+    @JsonProperty
+    private String master;
+
+    @JsonProperty
+    private String worker;
+
+    public String getMaster() {
+        return master;
+    }
+
+    public String getWorker() {
+        return worker;
     }
 }
 
@@ -23,10 +74,6 @@ class Etcd {
 
     public List<Endpoint> getEndpoints() {
         return endpoints;
-    }
-
-    public void setEndpoints(List<Endpoint> endpoints) {
-        this.endpoints = endpoints;
     }
 }
 
@@ -41,15 +88,7 @@ class Endpoint {
         return hostname;
     }
 
-    public void setHostname(String hostname) {
-        this.hostname = hostname;
-    }
-
     public Integer getPort() {
         return port;
-    }
-
-    public void setPort(Integer port) {
-        this.port = port;
     }
 }
