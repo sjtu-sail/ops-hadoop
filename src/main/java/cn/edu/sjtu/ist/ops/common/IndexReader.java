3252c226cdf06c27123b2d70cac5f8b5bd1cc7ae
Wu, ChungHsuan <wuchunghsuan@gmail.com>
2018-11-30 16:29:08 +0800

    Add IndexReader

---
+++ src/main/java/cn/edu/sjtu/ist/ops/common/IndexReader.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018 SJTU IST Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.edu.sjtu.ist.ops.common;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IndexReader {
+    private static final Logger logger = LoggerFactory.getLogger(IndexReader.class);
+    public static final int MAP_OUTPUT_INDEX_RECORD_LENGTH = 24;
+
+    private final File indexFile;
+    private final int partitions;
+    private final int size;
+    private LongBuffer entries;
+
+    public IndexReader(String indexFilePath) {
+        this.indexFile = new File(indexFilePath);
+        long length = this.indexFile.length();
+        this.partitions = (int) length / MAP_OUTPUT_INDEX_RECORD_LENGTH;
+        this.size = partitions * MAP_OUTPUT_INDEX_RECORD_LENGTH;
+
+        try {
+            ByteBuffer buf = ByteBuffer.allocate(size);
+            DataInputStream dis = new DataInputStream(new FileInputStream(this.indexFile));
+            readFully(dis, buf.array(), 0, size);
+            this.entries = buf.asLongBuffer();
+            logger.debug("Index file: " + indexFilePath + " size: " + this.size + " partitions: " + this.partitions);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            // TODO: handle exception
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+
+    public IndexRecord getIndex(int partition) {
+        final int pos = partition * MAP_OUTPUT_INDEX_RECORD_LENGTH / 8;
+        return new IndexRecord(entries.get(pos), entries.get(pos + 1), entries.get(pos + 2));
+    }
+
+    public static void readFully(InputStream in, byte[] buf, int off, int len) throws IOException {
+        int toRead = len;
+        while (toRead > 0) {
+            int ret = in.read(buf, off, toRead);
+            if (ret < 0) {
+                throw new IOException("Premature EOF from inputStream");
+            }
+            toRead -= ret;
+            off += ret;
+        }
+    }
+}
---
+++ src/main/java/cn/edu/sjtu/ist/ops/common/IndexRecord.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 SJTU IST Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.edu.sjtu.ist.ops.common;
+
+public class IndexRecord {
+    private final long startOffset;
+    private final long rawLength;
+    private final long partLength;
+
+    public IndexRecord(long startOffset, long rawLength, long partLength) {
+        this.startOffset = startOffset;
+        this.rawLength = rawLength;
+        this.partLength = partLength;
+    }
+
+    public long getStartOffset() {
+        return this.startOffset;
+    }
+
+    public long getRawLength() {
+        return this.rawLength;
+    }
+
+    public long getPartLength() {
+        return this.partLength;
+    }
+}
